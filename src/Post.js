2915a16dc2b850484e64a8409a6d7b99054daf0c
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2021-09-14 23:40:11 -0700

    Add call to NASA's API

--- src/Post.js
+++ src/Post.js
@@ -1,9 +1,10 @@
 import './Post.css';
 
-const Post = () => {
+const Post = (props) => {
   return (
     <div className="post-wrapper">
-      <img src="https://images.unsplash.com/photo-1502134249126-9f3755a50d78?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="space" />
+      <img 
+        src="https://images.unsplash.com/photo-1502134249126-9f3755a50d78?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" alt="space" />
       
       <div className="post-content">
         <div className="likes"><span class="far fa-heart"></span></div>
--- src/PostFeed.js
+++ src/PostFeed.js
@@ -1,10 +1,30 @@
+import React from 'react';
 import './PostFeed.css';
 import Post from './Post';
+import axios from 'axios';
 
-const PostFeed = () => {
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const PostFeed = (props) => {
+  axios.get(`https://api.nasa.gov/planetary/apod`, {
+    params: {
+      count: 5,
+      api_key: apiKey,
+    }
+  })
+  .then(response => {
+    const photos = response.data;
+    const firstPhoto = photos[0];
+    console.log(firstPhoto.title);
+    props(firstPhoto);
+  })
+  .catch(error => {
+    console.log(error);
+  })
+
   return (
     <main className="post-grid">
-      <Post />
+      <Post firstPhoto/>
     </main>
   );
 }
