e607071ea499d4468c9cc5bdbd2bdf2cae7617d8
lauradotcom <63886661+lauradotcom@users.noreply.github.com>
2021-09-16 23:10:49 -0700

    Add handler function to like button that toggles its state and changes its appearance

--- src/LikeButton.js
+++ src/LikeButton.js
@@ -2,8 +2,32 @@ import React, { useState } from 'react';
 import './LikeButton.css';
 
 const LikeButton = () => {
+
+  const unliked = 'far fa-heart';
+  const liked = 'fas fa-heart';
+
+  const [like, setLike] = useState(false);
+  const [heartClass, setHeartClass] = useState(unliked);
+  console.log(like);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    if (like === false) {
+      setLike(true);
+      setHeartClass(liked);
+    } else {
+      setLike(false);
+      setHeartClass(unliked);
+    }
+  }
+
   return(
-    <span class="far fa-heart"></span>
+    <button 
+      className={heartClass} 
+      aria-label="like"
+      onClick={handleClick}
+    >
+    </button>
   )
 }
 
